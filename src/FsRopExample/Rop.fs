025eb81bdfe0583e7efddd167f111aa758152ddb
Oskar Gewalli <gewalli@gmail.com>
2016-02-14 14:01:39 +0100

    Alias only

--- src/FsRopExample/Rop.fs
+++ src/FsRopExample/Rop.fs
@@ -1,22 +1,24 @@
 ﻿module FsRopExample.Rop
 
+type Result<'TSuccess, 'TError> =
+    | Success of 'TSuccess 
+    | Failure of 'TError
+
 /// A Result is a success or failure
 /// The Success case has a success value, plus a list of messages
 /// The Failure case has just a list of messages
-type RopResult<'TSuccess, 'TMessage> =
-    | Success of 'TSuccess * 'TMessage list
-    | Failure of 'TMessage list
+type RopResult<'TSuccess, 'TMessage> = Result<'TSuccess*'TMessage list,'TMessage list>
 
 /// create a Success with no messages
-let succeed x =
+let succeed x : RopResult<_,_>=
     Success (x,[])
 
 /// create a Success with a message
-let succeedWithMsg x msg =
+let succeedWithMsg x msg : RopResult<_,_>=
     Success (x,[msg])
 
 /// create a Failure with a message
-let fail msg =
+let fail msg : RopResult<_,_>=
     Failure [msg]
 
 /// A function that applies either fSuccess or fFailure 
@@ -26,7 +28,7 @@ let either fSuccess fFailure = function
     | Failure errors -> fFailure errors 
 
 /// merge messages with a result
-let mergeMessages msgs result =
+let mergeMessages msgs result : RopResult<_,_>=
     let fSuccess (x,msgs2) = 
         Success (x, msgs @ msgs2) 
     let fFailure errs = 
@@ -36,7 +38,7 @@ let mergeMessages msgs result =
 /// given a function that generates a new RopResult
 /// apply it only if the result is on the Success branch
 /// merge any existing messages with the new result
-let bindR f result =
+let bindR f result : RopResult<_,_>=
     let fSuccess (x,msgs) = 
         f x |> mergeMessages msgs
     let fFailure errs = 
@@ -46,7 +48,7 @@ let bindR f result =
 /// given a function wrapped in a result
 /// and a value wrapped in a result
 /// apply the function to the value only if both are Success
-let applyR f result =
+let applyR f result : RopResult<_,_>=
     match f,result with
     | Success (f,msgs1), Success (x,msgs2) -> 
         (f x, msgs1@msgs2) |> Success 
@@ -61,22 +63,22 @@ let (<*>) = applyR
 
 /// given a function that transforms a value
 /// apply it only if the result is on the Success branch
-let liftR f result =
+let liftR f result : RopResult<_,_>=
     let f' =  f |> succeed
     applyR f' result 
 
 /// given two values wrapped in results apply a function to both
-let lift2R f result1 result2 =
+let lift2R f result1 result2 : RopResult<_,_>=
     let f' = liftR f result1
     applyR f' result2 
 
 /// given three values wrapped in results apply a function to all
-let lift3R f result1 result2 result3 =
+let lift3R f result1 result2 result3 : RopResult<_,_>=
     let f' = lift2R f result1 result2 
     applyR f' result3
 
 /// given four values wrapped in results apply a function to all
-let lift4R f result1 result2 result3 result4 =
+let lift4R f result1 result2 result3 result4 : RopResult<_,_>=
     let f' = lift3R f result1 result2 result3 
     applyR f' result4
 
@@ -88,7 +90,7 @@ let mapR = liftR
 
 /// given an RopResult, call a unit function on the success branch
 /// and pass thru the result
-let successTee f result = 
+let successTee f result : RopResult<_,_>= 
     let fSuccess (x,msgs) = 
         f (x,msgs)
         Success (x,msgs) 
@@ -97,7 +99,7 @@ let successTee f result =
 
 /// given an RopResult, call a unit function on the failure branch
 /// and pass thru the result
-let failureTee f result = 
+let failureTee f result : RopResult<_,_>= 
     let fSuccess (x,msgs) = Success (x,msgs) 
     let fFailure errs = 
         f errs
@@ -105,7 +107,7 @@ let failureTee f result =
     either fSuccess fFailure result
 
 /// given an RopResult, map the messages to a different error type
-let mapMessagesR f result = 
+let mapMessagesR f result : RopResult<_,_>= 
     match result with 
     | Success (x,msgs) -> 
         let msgs' = List.map f msgs
